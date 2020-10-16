aac00dec5054e16a636afd167f64f99a63b02850
admin <pzharyuk@gmail.com>
2020-10-15 23:08:48 -0700

    Removed static URL entry and replaced it with dynamic ENV VAR used from .env

--- nb_helper.py
+++ nb_helper.py
@@ -2,21 +2,25 @@ import pynetbox
 import requests
 from requests.auth import HTTPBasicAuth
 import urllib3
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# DISABLE CERT WARNINGS/CHECKS FOR SELF SIGNED CERTS #
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
 session = requests.Session()
 session.verify = False
 
+# CONNECT TO DEV/PRD NETBOX ENVIROMENT #
 nb_dev = pynetbox.api(
-    'http://dev_netbox_url',
-    token='token goes here'
+    os.getenv("DEV_URL"),
+    token=os.getenv("DEV_TOKEN") # TOKEN IS LOADED INTO ENV FROM .env FILE #
 )
-nb_dev.http_session = session
 
 
 nb_prd = pynetbox.api(
-    'https://prod url goes here', 
-    token='token goes here'
+    os.getenv("PRD_URL"), 
+    token=os.getenv("PRD_TOKEN") # TOKEN IS LOADED INTO ENV FROM .env FILE #
 )
-nb_prd.http_session = session
+nb_prd.http_session = session # USED WITH HTTPS #
