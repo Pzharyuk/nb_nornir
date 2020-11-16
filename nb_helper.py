712023ca432448d37aed92805d0a75b66c590d88
devnetninja <pzharyuk@gmail.com>
2020-11-16 11:45:24 -0800

    Update nb_helper.py

--- nb_helper.py
+++ nb_helper.py
@@ -14,7 +14,7 @@ session.verify = False
 
 # CONNECT TO DEV NETBOX ENVIROMENT #
 nb_prd = pynetbox.api(
-    os.getenv("DEV_URL"), 
-    token=os.getenv("DEV_TOKEN") # TOKEN IS LOADED INTO ENV FROM .env FILE #
+    os.getenv("NB_URL"), 
+    token=os.getenv("NB_TOKEN") # TOKEN IS LOADED INTO ENV FROM .env FILE #
 )
 nb_prd.http_session = session # USED WITH HTTPS #
